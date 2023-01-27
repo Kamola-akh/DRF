725e19c5504c9dbc6075a11569d75cb9b7ea1db9
kamola-2002 <kamolaakhmadalieva2002@gmail.com>
2023-01-27 21:40:48 +0500

    requirements added

--- women/models.py
+++ women/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class WomenPost(models.Model):
     title = models.CharField(max_length=222)
     content = models.TextField()
-    img = models.ImageField(upload_to='womanPost')
+    img = models.ImageField(upload_to='WomanPostImg', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- women/urls.py
+++ women/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
-    path('posts/create', PostCreate.as_view()),
+    path('posts/create/', PostCreate.as_view()),
     path('posts/retrieve/<int:pk>', PostRetrieve.as_view()),
     path('posts/destroy/<int:pk>', PostDestroy.as_view()),
     path('posts/list-create/', PostListCreate.as_view()),
