2dcc167407ff3d6905d9a860bbb92e165dc7a74e
kamola-2002 <kamolaakhmadalieva2002@gmail.com>
2023-02-01 13:00:18 +0500

    swagger added

--- configuration/urls.py
+++ configuration/urls.py
@@ -18,12 +18,42 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from women import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DRF Blog API",
+        default_version="v1",
+        description="Blog API using Django rest framework",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 router = DefaultRouter()
-router.register(r'posts', views.PostList, basename='posts')
+router.register(r'posts', views.PostList)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('woman/', include('women.urls')),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 
 
 
